0a9866b8387987cfce9869ab815eb3aa1618d492
ooaaqq <eeviriyi@outlook.com>
2026-09-11 16:23:36 +0800

    Add reusable QQ bot container module (#6)
    Co-authored-by: ooaaqq <ooaaqq@users.noreply.github.com>

---
+++ modules/qq-bot.nix
@@ -0,0 +1,39 @@
+{ config, lib, ... }:
+let
+  cfg = config.ssvgg.qqBot;
+in
+{
+  options.ssvgg.qqBot = {
+    enable = lib.mkEnableOption "LLBot QQ bot";
+    image = lib.mkOption {
+      type = lib.types.str;
+      default = "linyuchen/llbot:v8.1.10";
+    };
+    dataDir = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/llbot";
+    };
+    webuiPort = lib.mkOption {
+      type = lib.types.port;
+      default = 3080;
+    };
+    milkyPort = lib.mkOption {
+      type = lib.types.port;
+      default = 3000;
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    virtualisation.podman.enable = true;
+    virtualisation.oci-containers.backend = "podman";
+    virtualisation.oci-containers.containers.llbot = {
+      image = cfg.image;
+      ports = [
+        "127.0.0.1:${toString cfg.webuiPort}:3080"
+        "127.0.0.1:${toString cfg.milkyPort}:3000"
+      ];
+      volumes = [ "${cfg.dataDir}:/app/llbot/data:rw" ];
+      extraOptions = [ "--pull=missing" ];
+    };
+    systemd.tmpfiles.rules = [ "d ${cfg.dataDir} 0750 root root -" ];
+  };
+}
